60dd77dc34476ff384d1d4fe94d586eccbee92c1
Rasmi Jati <jatirasmi65@gmail.com>
2024-03-24 20:55:41 +0545

    displayed binary representation and grouping of it along with decoded text

--- src/messageencoderdecoder/Decoder.java
+++ src/messageencoderdecoder/Decoder.java
@@ -13,7 +13,7 @@ public class Decoder {
 
     public Decoder() {
     }
-       
+
     public String decodeBase64(String encodedText) {
         StringBuilder binaryString = new StringBuilder();
         for (char c : encodedText.toCharArray()) {
@@ -25,13 +25,22 @@ public class Decoder {
             binaryString.append(binary);
         }
 
+        System.out.println("=======================================================================");
+        System.out.println("Binary Representation for \"" + encodedText + "\":");
+        System.out.println(binaryString.toString().trim());
+        System.out.println("=======================================================================");
+
         StringBuilder decodedString = new StringBuilder();
+        System.out.println("Grouped Binary Representation:");
         for (int i = 0; i < binaryString.length(); i += 8) {
             String group = binaryString.substring(i, Math.min(i + 8, binaryString.length()));
             int decimalValue = Integer.parseInt(group, 2);
             char decodedChar = (char) decimalValue;
             decodedString.append(decodedChar);
+            System.out.print(group.trim() + "\t"); // Trim to remove the trailing space
         }
+        System.out.println("");
+        System.out.println("=======================================================================");
 
         return decodedString.toString();
     }
