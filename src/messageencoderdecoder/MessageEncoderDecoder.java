87e9084f4228423566cae20f1e3247549e764c51
Rasmi Jati <jatirasmi65@gmail.com>
2024-03-24 20:42:29 +0545

    formated messageencoderdecoder class

--- src/messageencoderdecoder/MessageEncoderDecoder.java
+++ src/messageencoderdecoder/MessageEncoderDecoder.java
@@ -12,36 +12,41 @@ import java.util.Scanner;
  * @author admin
  */
 public class MessageEncoderDecoder {
-    
+
     public static void main(String[] args) {
         Encoder encode = new Encoder();
         Decoder decode = new Decoder();
-        
+
         Scanner sc = new Scanner(System.in);
         while (true) {
-            System.out.println("*********************************");
-            System.out.println("Choose either to Encoder or Decoder");
-            System.out.println("*********************************");
+            System.out.println("###################################");
+            System.out.println(" Message Encoding and Decoding ");
+            System.out.println("###################################");
             System.out.println("1. Enter 1 for Encode");
             System.out.println("2. Enter 2 for Decode");
             System.out.println("3. Enter 3 to Exit");
+            System.out.println("-----------------------------------");
             System.out.print("Enter your choice : ");
-
             String choice = sc.next();
+            System.out.println("------------------------------------");
             sc.nextLine();
 
             switch (choice) {
                 case "1":
-                    System.out.print("Enter the plain text to encode: ");
+                    System.out.println("Enter the plain text to encode: ");
                     String plainText = sc.nextLine();
                     String encodedText = encode.encodeBase64(plainText);
-                    System.out.println("Encoded Text: " + encodedText);
+                    System.out.println("Encoded Text: ");
+                    System.out.println(encodedText);
+                    System.out.println("");
                     break;
                 case "2":
-                    System.out.print("Enter the Base 64 encoded text to decode: ");
+                    System.out.println("Enter the Base 64 encoded text to decode: ");
                     String encodedTextToDecode = sc.nextLine();
                     String decodedText = decode.decodeBase64(encodedTextToDecode);
-                    System.out.println("Decoded Text: " + decodedText);
+                    System.out.println("Decoded Text: ");
+                    System.out.println(decodedText);
+                    System.out.println("");
                     break;
                 case "3":
                     System.out.println("Exiting the program. Goodbye!");
